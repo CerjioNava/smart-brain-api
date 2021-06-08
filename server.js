d28614a724564737c3f6d97ffedffab0f9784805
unknown <cerjionava@gmail.com>
2021-06-08 19:37:24 -0400

    Nailed the connection with the database. At least for basic knowledge and experience, still a lot to improve

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ const db = knex({
 
 // console.log(postgres.select('*').from('users'));
 db.select('*').from('users').then(data => {         // Query 
-    console.log(data);
+    console.log('We have', data.length, 'users!');
 });
 
 
@@ -61,87 +61,91 @@ const database = {
 
 // HOME PAGE GET
 app.get('/', (req, res) => {
-    // res.send('This is working');
     res.send(database.users);
 });
 
 // SIGN IN POST
 app.post('/signin', (req, res) => {
-    // Load hash from your password DB.
-    // bcrypt.compare("apple", '$2a$10$0/VMkaFrCMh15yU0WMPJeeRA/xRzIc1982GOmsDtMFwvHkGhGOJai', function(err, res) {
-    //     // res == true
-    //     console.log('First guess', res)
-    // });
-    // bcrypt.compare("veggies", '$2a$10$0/VMkaFrCMh15yU0WMPJeeRA/xRzIc1982GOmsDtMFwvHkGhGOJai', function(err, res) {
-    //     // res = false
-    //     console.log('Second guess', res)
-    // });
-
-    if (req.body.email === database.users[0].email &&           // Comparamos usuario
-         req.body.password === database.users[0].password) {
-        //res.json("success");
-        res.json(database.users[0]);
-    } else {
-        res.status(400).json("error logging in");
-    }
+    db.select('email', 'hash').from('login')
+      .where('email', '=', req.body.email)
+      .then(data => {
+        console.log(data[0])
+        const isValid = bcrypt.compareSync(req.body.password, data[0].hash);    // if true
+        if (isValid) {
+          return db.select('*').from('users')
+            .where('email', '=', req.body.email)
+            .then(user => {
+                res.json(user[0])
+            })
+            .catch(err => res.status(400).json('Unable to get user'))
+        } else {
+          res.status(400).json('Wrong credentials');
+        }
+      }) 
+      .catch(err => res.status(400).json('Wrong credentials'))         
 });
 
 // REGISTER POST
 app.post('/register', (req, res) => {
     const { email, name, password } = req.body;     // Obtenemos la data del body
-    db('users')
-      .returning('*')  
-      .insert({
-        email: email,
-        name: name,   
-        joined: new Date()         
-      })
-      .then(user => {
-        res.json(user[0]); 
-      })
-      .catch(err => res.status(400).json('unable to register'));
+    const hash = bcrypt.hashSync(password);
 
-    // bcrypt.hash(password, null, null, function(err, hash) {      // Hash de password
-    //     console.log(hash);
-    // });
-
-    // database.users.push({                           // Añadimos a la base de datos con el body
-    //     id: '125',
-    //     name: name,
-    //     email: email,
-    //     //password: password,
-    //     entries: 0,
-    //     joined: new Date()        
-    // });
-    // res.json(database.users[database.users.length-1]);     // Devolvemos un response con el nuevo
+    db.transaction(trx => {
+        trx.insert({
+            hash: hash,
+            email: email
+        })
+        .into("login")
+        .returning('email')
+        .then(loginEmail => {
+            return trx('users')
+                .returning('*')  
+                .insert({
+                    email: loginEmail[0],
+                    name: name,   
+                    joined: new Date()         
+                })
+                .then(user => {
+                    res.json(user[0]); 
+                })                
+        })
+        .then(trx.commit)
+        .catch(trx.rollback)
+    })    
+    .catch(err => res.status(400).json('unable to register'));
 });
 
 // PROFILE GET
 app.get('/profile/:id', (req, res) => {
     const { id } = req.params                       // Obtiene los parametros del "id" desde el URL.    
-    database.users.forEach(user => {                // Loop en los usuarios
-        if (user.id === id) {
-            return res.json(user);                  // Si existe, devuelve usuario
-        } 
-    })
-    res.status(404).json('no such user');           // Si no existe, 404.
+    
+    db.select('*').from('users').where({id: id})
+        .then(user => {
+            if (user.length) {
+                res.json(user[0]);
+            } else {
+                res.status(400).json('Not Found')
+            }
+        })
+        .catch(err => res.status(400).json('Error getting user'))    
 });
 
 // IMAGE PUT
 app.put('/image', (req, res) => {
     const { id } = req.body                       // Obtiene los parametros del "id" del BODY  
-    database.users.forEach(user => {              // Loop en los usuarios al entrar
-        if (user.id === id) {
-            user.entries++;                       // Si existe, se aumenta el conteo de ingresos
-            return res.json(user.entries);        // Devolvemos las entradas del user
-        } 
-    })
-    res.status(404).json('no such user');           // Si no existe, 404.
+    
+    db('users').where('id', '=', id)
+        .increment('entries', 1)
+        .returning('entries')
+        .then(entries => {
+            res.json(entries[0])
+        })
+        .catch(err => res.status(400).json('Unable to get entries'))
 });
 
 
 // ----------------------------------------------------------------
-// Extraido de la página de bcrypt-nodejs para hacer hash en las passwords.
+// Extraido de la página de bcrypt-nodejs para hacer hash en las passwords. (ASINCRONO)
 
 // bcrypt.hash("bacon", null, null, function(err, hash) {
 //     // Store hash in your password DB.
@@ -155,8 +159,15 @@ app.put('/image', (req, res) => {
 //     // res = false
 // });
 
+// SINCRONO
+
+// var hash = bcrypt.hashSync("bacon");
+
+// bcrypt.compareSync("bacon", hash); // true
+// bcrypt.compareSync("veggies", hash); // false
+
 // ----------------------------------------------------------------
 
 app.listen(3001, () => {
-   console.log('app is running on port 3000');
+   console.log('app is running on port 3001');
 });
